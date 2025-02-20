2fa1ca7d390156dffc5762a5b8223464d891f87a
PC <PC@PC>
2025-02-20 10:28:03 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlashHack_Foxtrot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
